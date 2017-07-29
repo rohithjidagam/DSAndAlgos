7df5013f7ecfbf3bbc17c37dd0d8c7c333e93f0a
Rohith Jidagam <rjidagam@ALMBP15-00018.local>
2017-07-29 12:00:50 -0500

    removed libs and target

--- src/main/java/com/uh/ConflictingAppointments.java
+++ src/main/java/com/uh/ConflictingAppointments.java
@@ -8,15 +8,44 @@ public class ConflictingAppointments {
 
         INode[] interval = { new INode(1, 5), new INode(3, 7), new INode(2, 6), new INode(10, 15), new INode(5, 6),
                 new INode(4, 100) };
-        root = null;
         int n = interval.length;
         printConflicts(interval, n);
     }
 
     private static void printConflicts(INode[] interval, int n) {
 
+        root = null;
         insert(interval[0], root);
 
+        for (int i = 1; i < n; i++) {
+
+            INode res = overLap(root, interval[i]);
+            if (res != null) {
+                System.out.println(interval[i] + " conflicts with" + res);
+            }
+            
+            root = insert(interval[i], root);
+        }
+
+    }
+
+    static boolean doOVerlap(INode i1, INode i2) {
+        if (i1.low < i2.high && i2.low < i1.high)
+            return true;
+        return false;
+    }
+
+    private static INode overLap(ITree root, INode iNode) {
+        if (root == null)
+            return null;
+
+        if (doOVerlap((root.i), iNode))
+            return root.i;
+
+        if (root.left != null && root.left.max >= iNode.low)
+            return overLap(root.left, iNode);
+
+        return overLap(root.right, iNode);
     }
 
     private static ITree insert(INode iNode, ITree node) {
@@ -25,19 +54,19 @@ public class ConflictingAppointments {
             return node;
         }
 
-        int l = root.i.low;
+        int l = node.i.low;
 
         if (iNode.low < l) {
-            root.left = insert(root.left.i, node);
+            node.left = insert(iNode, node.left);
         } else {
-            root.right = insert(root.right.i, node);
-        }
-        
-        if(root.max < node.max){
-            
+            node.right = insert(iNode, node.right);
         }
 
-        return root;
+        if (node.max < iNode.high) {
+            node.max = iNode.high;
+        }
+
+        return node;
 
     }
 
@@ -63,5 +92,10 @@ class INode {
     INode(int l, int h) {
         this.low = l;
         this.high = h;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + low + "," + high + "]";
     }
 }

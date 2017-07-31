312cb82475ecac0ea320b0131fe5387befbe46d2
Rohith Jidagam <rjidagam@ALMBP15-00018.local>
2017-07-31 15:51:03 -0500

    Added backtracking examples

--- src/main/java/com/uh/BackTrackingExamples.java
+++ src/main/java/com/uh/BackTrackingExamples.java
@@ -27,8 +27,74 @@ public class BackTrackingExamples {
         boolean[] used = new boolean[nums3.length];
         permutationsWithDups(lists, new ArrayList<>(), nums3, used);
         print(lists);
-        
-        
+
+        int[] arr = { 2, 3, 6, 7 };
+        int sum = 7;
+        combinations(lists, new ArrayList<>(), arr, sum, 0);
+        print(lists);
+
+        int[] arr2 = { 10, 1, 2, 7, 6, 1, 5 };
+        sum = 8;
+        Arrays.sort(arr2);
+        combinationsUnique(lists, new ArrayList<>(), arr2, sum, 0);
+        print(lists);
+
+        String s = "aab";
+        palindromePartitions(lists, new ArrayList<>(), s, 0);
+        print(lists);
+
+    }
+
+    private static void palindromePartitions(List<List<Integer>> lists, ArrayList temp, String s, int st) {
+        if (st == s.length()) {
+            lists.add(new ArrayList(temp));
+        } else
+            for (int j = st; j < s.length(); j++) {
+                if (isPalindrome(s, st, j)) {
+                    temp.add(s.substring(st, j + 1));
+                    palindromePartitions(lists, temp, s, j + 1);
+                    temp.remove(temp.size() - 1);
+                }
+            }
+    }
+
+    private static boolean isPalindrome(String s, int low, int high) {
+        while (low < high)
+            if (s.charAt(low++) != s.charAt(high--))
+                return false;
+        return true;
+    }
+
+    private static void combinationsUnique(List<List<Integer>> lists, ArrayList temp, int[] arr, int sum, int i) {
+
+        if (sum < 0)
+            return;
+        else if (sum == 0)
+            lists.add(new ArrayList(temp));
+        else {
+            for (int j = i; j < arr.length; j++) {
+                if (j > i && arr[j] == arr[j - 1])
+                    continue;
+                temp.add(arr[j]);
+                combinationsUnique(lists, temp, arr, sum - arr[j], j + 1);
+                temp.remove(temp.size() - 1);
+            }
+        }
+
+    }
+
+    private static void combinations(List<List<Integer>> lists, ArrayList temp, int[] arr, int sum, int i) {
+        if (sum < 0)
+            return;
+        else if (sum == 0)
+            lists.add(new ArrayList(temp));
+        else {
+            for (int j = i; j < arr.length; j++) {
+                temp.add(arr[j]);
+                combinations(lists, temp, arr, sum - arr[j], j);
+                temp.remove(temp.size() - 1);
+            }
+        }
     }
 
     private static void permutationsWithDups(List<List<Integer>> lists, ArrayList temp, int[] nums, boolean[] used) {
@@ -36,7 +102,7 @@ public class BackTrackingExamples {
             lists.add(new ArrayList(temp));
         } else
             for (int j = 0; j < nums.length; j++) {
-                if(used[j] || j>0 &&nums[j] == nums[j-1] && !used[j-1])
+                if (used[j] || j > 0 && nums[j] == nums[j - 1] && !used[j - 1])
                     continue;
                 used[j] = true;
                 temp.add(nums[j]);

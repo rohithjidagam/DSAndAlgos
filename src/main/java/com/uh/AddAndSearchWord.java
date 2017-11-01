1519c8edcfce357fe90eee064f6133e94158b0bf
Rohith Jidagam <rjidagam@ets.org>
2017-11-01 18:47:57 -0500

    Added examples

--- src/main/java/com/uh/AddAndSearchWord.java
+++ src/main/java/com/uh/AddAndSearchWord.java
@@ -9,6 +9,8 @@ public class AddAndSearchWord {
 
     Map<Integer, List<String>> map = new HashMap<>();
 
+    static TTNode root = null;
+
     public static void main(String[] args) {
 
         AddAndSearchWord se = new AddAndSearchWord();
@@ -21,7 +23,57 @@ public class AddAndSearchWord {
         System.out.println(se.search("..d"));
         System.out.println(se.search("cad"));
         System.out.println(se.search("..."));
-        
+
+        System.out.println("********");
+        // Trie Method
+        root = new TTNode();
+        add("bad");
+        add("mad");
+        add("dad");
+        System.out.println(find("b.."));
+        System.out.println(find("b.d"));
+        System.out.println(find(".a."));
+        System.out.println(find("..d"));
+        System.out.println(find("cad"));
+        System.out.println(find("..."));
+
+    }
+
+    private static boolean find(String s) {
+
+        return find(s.toCharArray(), 0, root);
+    }
+
+    private static boolean find(char[] ch, int i, TTNode cur) {
+
+        if (i == ch.length)
+            return cur.isLeaf;
+
+        if (ch[i] == '.') {
+            for (int j = 0; j < 26; j++) {
+                if (cur.child[j] != null) {
+                    return find(ch, i + 1, cur.child[j]);
+                }
+            }
+        } else {
+            if (cur.child[ch[i] - 'a'] == null)
+                return false;
+            else
+                return find(ch, i + 1, cur.child[ch[i] - 'a']);
+        }
+        return false;
+    }
+
+    private static void add(String s) {
+
+        TTNode cur = root;
+        for (int i = 0; i < s.length(); i++) {
+            int ch = s.charAt(i) - 'a';
+            if (cur.child[ch] == null)
+                cur.child[ch] = new TTNode();
+            cur = cur.child[ch];
+        }
+        cur.isLeaf = true;
     }
 
     public void addWord(String word) {
@@ -59,4 +111,18 @@ public class AddAndSearchWord {
         return true;
     }
 
+}
+
+class TTNode {
+    TTNode[] child;
+    boolean isLeaf;
+    int N = 26;
+
+    public TTNode() {
+        isLeaf = false;
+        child = new TTNode[N];
+        for (int i = 0; i < child.length; i++) {
+            child[i] = null;
+        }
+    }
 }
